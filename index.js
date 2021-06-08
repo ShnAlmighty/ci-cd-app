371355eccdbeca8a7b90a6252fb92dc409e9068f
shanalmighty <patnaikshantanu@gmail.com>
2021-06-08 10:15:11 +0530

    another commit

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.get('/',(req,res)=>{
+    res.send('hello world');
+})
+
 app.listen(port,(err,res)=>{
     console.log('server running at port:',port);
 })
