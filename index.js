9380a50e61d48c3719d27c81177488fc2935df7b
shanalmighty <patnaikshantanu@gmail.com>
2021-06-09 17:12:44 +0530

    final v1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/',(req,res)=>{
-    res.send('Hello world this is ec2 speaking and chilling, Cheers mate!');
+    res.send('Hello world this is ec2 speakin and chillin, Cheers mate!');
 })
 
 app.listen(port,(err,res)=>{
