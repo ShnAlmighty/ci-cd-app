d8b3f43ec636fbfeaa6921452d5e20a0b6488cf5
shanalmighty <patnaikshantanu@gmail.com>
2021-06-09 10:14:35 +0530

    release v18

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/',(req,res)=>{
-    res.send('Hello world this is ec2 speakin and chillin.');
+    res.send('Hello world this is ec2 speaking and chillin.');
 })
 
 app.listen(port,(err,res)=>{
